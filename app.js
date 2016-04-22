3819cc0fffa92178017903202ebd9688cf793df6
Yibo Gou <i@bowiegou.com>
2016-04-21 22:32:33 -0400

    modified relative path

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-
+var path = require('path');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
@@ -16,7 +16,8 @@ var cfenv = require('cfenv');
 var app = express();
 
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public/BrickStep/')));
+
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
