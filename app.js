4c114b7e2abc23bf41dfcb415ca802a7d0b99720
Yibo Gou <i@bowiegou.com>
2016-04-26 04:12:51 -0400

    get cred from env

--- app.js
+++ app.js
@@ -25,9 +25,16 @@ app.use('/getscore', getscore);
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+var dbCred = appEnv.getServiceCreds('ClearDB MySQL Database-q0');
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
     process.env['score_id'] = 2;
+    process.env['dbURL'] = dbCred['hostname'];
+    process.env['dbPort'] = dbCred['port'];
+    process.env['dbUsername'] = dbCred['username'];
+    process.env['dbPassword'] = dbCred['password'];
+    console.log(dbCred);
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
